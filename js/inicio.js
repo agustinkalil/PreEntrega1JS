89a7c3e9162ad4b3b313330382241efc1373c7bf
agustin bazan <agustinbazan066@gmail.com>
2023-09-17 20:54:18 -0300

    Eliminar archivos que ya no son necesarios

--- js/inicio.js
+++ js/inicio.js
@@ -1,4 +1,96 @@
-import { comprarProducto } from "./carrito.js";
+let divProductos = document.getElementById("productos");
+
+const getBeatsInformacion = async () => {
+    let respuesta = await fetch("./data.json");
+    let data = await respuesta.json();
+    let informacion = data;
+
+    informacion.forEach(item => {
+        let card = document.createElement("div");
+        card.className = "card mb-1"; 
+    card.style = "max-width: 18rem;";
+        card.innerHTML = `
+        <img src="${item.imagen}" class="card-img-top" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">${item.nombre}</h5>
+            <p class="card-text">Bpm: ${item.bpm}</p>
+            <p class="card-text precio"><b>$${item.precio}</b></p>
+            <button id="Comprar${item.id}" class="btn btn-danger">Comprar</button>
+        </div>
+    `;
+
+        divProductos.append(card);
+
+
+        let btnComprar = document.getElementById(`Comprar${item.id}`);
+
+        btnComprar.addEventListener("click", () => {
+
+            item.id === 1 ?
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Este beat no está disponible',
+                    text: 'Lo sentimos, este beat no está disponible para su compra.'
+                })
+            : Swal.fire({
+                icon: 'success',
+                title: 'El beat se agregó al carrito',
+                text: 'El beat se ha agregado exitosamente a tu carrito de compras.'
+            });}
+    
+        )
+    })
+
+}
+
+getBeatsInformacion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import { comprarProducto } from "./carrito.js";
 
 const divProductos = document.getElementById("productos");
 
@@ -38,4 +130,4 @@ export const generarCardsBeats = (productos) => {
         btnComprar.addEventListener("click", () => comprarProducto(id))
 
     });
-}
+} */
